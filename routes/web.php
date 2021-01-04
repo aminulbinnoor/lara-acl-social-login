411504b7ba9e68a5c6a9c5a4d22ef87f55d17556
aminulbinnoor <cselab7777@email.com>
2021-01-04 16:12:55 +0600

    lara role permission social login admin section

---
+++ routes/web.php
@@ -0,0 +1,122 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Homepage Route
+Route::group(['middleware' => ['web', 'checkblocked']], function () {
+    Route::get('/', 'WelcomeController@welcome')->name('welcome');
+    Route::get('/terms', 'TermsController@terms')->name('terms');
+});
+
+// Authentication Routes
+Auth::routes();
+
+// Public Routes
+Route::group(['middleware' => ['web', 'activity', 'checkblocked']], function () {
+
+    // Activation Routes
+    Route::get('/activate', ['as' => 'activate', 'uses' => 'Auth\ActivateController@initial']);
+
+    Route::get('/activate/{token}', ['as' => 'authenticated.activate', 'uses' => 'Auth\ActivateController@activate']);
+    Route::get('/activation', ['as' => 'authenticated.activation-resend', 'uses' => 'Auth\ActivateController@resend']);
+    Route::get('/exceeded', ['as' => 'exceeded', 'uses' => 'Auth\ActivateController@exceeded']);
+
+    // Socialite Register Routes
+    Route::get('/social/redirect/{provider}', ['as' => 'social.redirect', 'uses' => 'Auth\SocialController@getSocialRedirect']);
+    Route::get('/social/handle/{provider}', ['as' => 'social.handle', 'uses' => 'Auth\SocialController@getSocialHandle']);
+
+    // Route to for user to reactivate their user deleted account.
+    Route::get('/re-activate/{token}', ['as' => 'user.reactivate', 'uses' => 'RestoreUserController@userReActivate']);
+});
+
+// Registered and Activated User Routes
+Route::group(['middleware' => ['auth', 'activated', 'activity', 'checkblocked']], function () {
+
+    // Activation Routes
+    Route::get('/activation-required', ['uses' => 'Auth\ActivateController@activationRequired'])->name('activation-required');
+    Route::get('/logout', ['uses' => 'Auth\LoginController@logout'])->name('logout');
+});
+
+// Registered and Activated User Routes
+Route::group(['middleware' => ['auth', 'activated', 'activity', 'twostep', 'checkblocked']], function () {
+
+    //  Homepage Route - Redirect based on user role is in controller.
+    Route::get('/home', ['as' => 'public.home',   'uses' => 'UserController@index']);
+
+    // Show users profile - viewable by other users.
+    Route::get('profile/{username}', [
+        'as'   => '{username}',
+        'uses' => 'ProfilesController@show',
+    ]);
+});
+
+// Registered, activated, and is current user routes.
+Route::group(['middleware' => ['auth', 'activated', 'currentUser', 'activity', 'twostep', 'checkblocked']], function () {
+
+    // User Profile and Account Routes
+    Route::resource(
+        'profile',
+        \ProfilesController::class,
+        [
+            'only' => [
+                'show',
+                'edit',
+                'update',
+                'create',
+            ],
+        ]
+    );
+    Route::put('profile/{username}/updateUserAccount', [
+        'as'   => '{username}',
+        'uses' => 'ProfilesController@updateUserAccount',
+    ]);
+    Route::put('profile/{username}/updateUserPassword', [
+        'as'   => '{username}',
+        'uses' => 'ProfilesController@updateUserPassword',
+    ]);
+    Route::delete('profile/{username}/deleteUserAccount', [
+        'as'   => '{username}',
+        'uses' => 'ProfilesController@deleteUserAccount',
+    ]);
+
+    // Route to show user avatar
+    Route::get('images/profile/{id}/avatar/{image}', [
+        'uses' => 'ProfilesController@userProfileAvatar',
+    ]);
+
+    // Route to upload user avatar.
+    Route::post('avatar/upload', ['as' => 'avatar.upload', 'uses' => 'ProfilesController@upload']);
+});
+
+// Registered, activated, and is admin routes.
+Route::group(['middleware' => ['auth', 'activated', 'role:admin', 'activity', 'twostep', 'checkblocked']], function () {
+    Route::resource('/users/deleted', \SoftDeletesController::class, [
+        'only' => [
+            'index', 'show', 'update', 'destroy',
+        ],
+    ]);
+
+    Route::resource('users', \UsersManagementController::class, [
+        'names' => [
+            'index'   => 'users',
+            'destroy' => 'user.destroy',
+        ],
+        'except' => [
+            'deleted',
+        ],
+    ]);
+    Route::post('search-users', 'UsersManagementController@search')->name('search-users');
+
+    Route::resource('themes', \ThemesManagementController::class, [
+        'names' => [
+            'index'   => 'themes',
+            'destroy' => 'themes.destroy',
+        ],
+    ]);
+
+    Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
+    Route::get('routes', 'AdminDetailsController@listRoutes');
+    Route::get('active-users', 'AdminDetailsController@activeUsers');
+});
+
+Route::redirect('/php', '/phpinfo', 301);
